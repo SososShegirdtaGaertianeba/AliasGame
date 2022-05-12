221dc61ef4baf48f62b05b4b7cd74c5aa5e7d309
ioane <ioane.kareli.1@btu.edu.ge>
2022-05-12 21:28:39 +0400

    add home fragment

--- app/src/main/java/com/example/alias/app/App.kt
+++ app/src/main/java/com/example/alias/app/App.kt
@@ -1,11 +1,18 @@
 package com.example.alias.app
 
 import android.app.Application
+import android.widget.Spinner
+import com.example.alias.R
 import com.example.alias.di.viewModels
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
+
+    companion object {
+        var LANGUAGE = "English"
+    }
+
 
     override fun onCreate() {
         super.onCreate()
@@ -13,6 +20,7 @@ class App : Application() {
             androidContext(this@App)
             modules(listOf(viewModels))
         }
+
     }
 
 }
