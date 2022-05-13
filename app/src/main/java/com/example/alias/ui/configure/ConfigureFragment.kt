254537efb3c50ea4abd08e84b24956a3aee22aff
AkoG1 <96634605+AkoG1@users.noreply.github.com>
2022-05-13 17:13:01 +0400

    add configure fragment

--- app/src/main/java/com/example/alias/ui/configure/ConfigureFragment.kt
+++ app/src/main/java/com/example/alias/ui/configure/ConfigureFragment.kt
@@ -1,11 +1,57 @@
 package com.example.alias.ui.configure
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.example.alias.databinding.ConfigureFragmentBinding
 import com.example.alias.ui.base.BaseFragment
+import com.example.alias.ui.configure.adapter.ConfigurationViewPagerAdapter
+import com.example.alias.ui.configure.view_pager_fragments.pager_game_mode.PagerGameModeFragment
+import com.example.alias.ui.configure.view_pager_fragments.pager_teams.PagerTeamsFragment
+import com.example.alias.ui.configure.view_pager_fragments.pager_time_and_points.PagerTimeAndPointsFragment
+import com.example.alias.ui.configure.vm.ConfigureViewModel
+import com.example.alias.util.PagingEvent
 
-class ConfigureFragment : BaseFragment<ConfigureFragmentBinding>(ConfigureFragmentBinding::inflate) {
+class ConfigureFragment :
+    BaseFragment<ConfigureFragmentBinding>(ConfigureFragmentBinding::inflate) {
+
+    private val viewModel: ConfigureViewModel by viewModels()
+
+    private lateinit var adapter: ConfigurationViewPagerAdapter
+
+    private val viewPager: ViewPager2
+        get() = binding.viewPager
+
     override fun init() {
-        TODO("Not yet implemented")
+        initViewPager()
+        initObservers()
+    }
+
+    private fun initViewPager() {
+        val fragmentList = arrayOf<Fragment>(
+            PagerGameModeFragment(),
+            PagerTeamsFragment(),
+            PagerTimeAndPointsFragment()
+        )
+        adapter = ConfigurationViewPagerAdapter(
+            fragmentList,
+            childFragmentManager,
+            viewLifecycleOwner.lifecycle,
+        )
+        viewPager.adapter = adapter
+
+    }
+
+    private fun initObservers() {
+        var i = 0
+        viewModel.events.forEach { observer ->
+            observer.observe(viewLifecycleOwner) {
+                if (it.isHandled) {
+                    viewPager.currentItem = it.position
+                    viewModel.events[i++].value = PagingEvent(it.data, it.position)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/alias/ui/configure/vm/ConfigureViewModel.kt
+++ app/src/main/java/com/example/alias/ui/configure/vm/ConfigureViewModel.kt
@@ -1,7 +1,42 @@
 package com.example.alias.ui.configure.vm
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.alias.util.GameMode
+import com.example.alias.util.PagingEvent
 
 class ConfigureViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val gameMode: GameMode = GameMode()
+
+    private val isClassic =
+        MutableLiveData(PagingEvent(true, 1))
+
+    private val teams =
+        MutableLiveData(PagingEvent(mutableListOf<String>(), 2))
+
+    private val timePerRoundAndPointsToWin =
+        MutableLiveData(PagingEvent(intArrayOf(30, 100), 3))
+
+    val events = arrayOf(isClassic, teams, timePerRoundAndPointsToWin)
+
+    fun setIsClassic(isClassic: Boolean) {
+        this.isClassic.value = PagingEvent(isClassic, 1, true)
+    }
+
+    fun setTeams(teams: MutableList<String>) {
+        this.teams.value = PagingEvent(teams, 2, true)
+    }
+
+    fun setTimePerRoundAndPointsToWin(timePerRound: Int, pointsToWin: Int) {
+        this.timePerRoundAndPointsToWin.value =
+            PagingEvent(intArrayOf(timePerRound, pointsToWin), 3, true)
+    }
+
+    fun initGameMode() {
+        gameMode.isClassic = isClassic.value?.data
+        gameMode.teams = teams.value?.data
+        gameMode.timePerRound = timePerRoundAndPointsToWin.value?.data?.get(0)
+        gameMode.pointsToWin = timePerRoundAndPointsToWin.value?.data?.get(1)
+    }
 }
