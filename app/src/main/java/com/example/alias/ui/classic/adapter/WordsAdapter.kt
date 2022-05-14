4da21f767a00f72a84d1f7a150bfd6f1d3d6f457
ioane <ioane.kareli.1@btu.edu.ge>
2022-05-14 14:04:18 +0400

    add classic mode adapter

--- app/src/main/java/com/example/alias/ui/classic/adapter/WordsAdapter.kt
+++ app/src/main/java/com/example/alias/ui/classic/adapter/WordsAdapter.kt
@@ -1,4 +1,43 @@
 package com.example.alias.ui.classic.adapter
 
-class WordsAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alias.databinding.ClassicModeWordItemBinding
+
+class WordsAdapter(
+    val onClick: () -> Unit
+): RecyclerView.Adapter<WordsAdapter.ViewHolder>() {
+
+    var items = mutableListOf<String>()
+
+    inner class ViewHolder(private val binding:ClassicModeWordItemBinding):RecyclerView.ViewHolder(binding.root){
+        fun onBind(position: Int){
+            val model = items[position]
+            binding.classicWordTv.text = model
+            itemView.setOnClickListener { onClick() }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ClassicModeWordItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.onBind(position)
+    }
+
+    override fun getItemCount() = items.size
+
+    fun setData(items: MutableList<String>){
+        this.items = items
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
 }
