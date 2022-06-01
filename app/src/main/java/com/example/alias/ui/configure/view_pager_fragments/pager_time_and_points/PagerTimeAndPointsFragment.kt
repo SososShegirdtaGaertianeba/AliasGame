86cf2b502fce8c3dcde8a30bf4e6e3d03ac066de
AkoG1 <96634605+AkoG1@users.noreply.github.com>
2022-06-01 21:56:38 +0400

    add CircularSeekBar

--- app/src/main/java/com/example/alias/ui/configure/view_pager_fragments/pager_time_and_points/PagerTimeAndPointsFragment.kt
+++ app/src/main/java/com/example/alias/ui/configure/view_pager_fragments/pager_time_and_points/PagerTimeAndPointsFragment.kt
@@ -5,6 +5,7 @@ import com.example.alias.R
 import com.example.alias.databinding.FragmentPagerTimeAndPointsBinding
 import com.example.alias.ui.configure.view_pager_fragments.base.BaseFragment
 import com.example.alias.ui.configure.vm.ConfigureViewModel
+import me.tankery.lib.circularseekbar.CircularSeekBar
 
 class PagerTimeAndPointsFragment :
     BaseFragment<FragmentPagerTimeAndPointsBinding>(FragmentPagerTimeAndPointsBinding::inflate) {
@@ -16,9 +17,86 @@ class PagerTimeAndPointsFragment :
     private var isToastHandled = false
 
     override fun init() {
-        initTextViews()
+        initTimeSeekBar()
+        initPointSeekbar()
         initOnClickListeners()
         initObservers()
+    }
+
+    private fun initPointSeekbar() {
+        val pointSeekBar = binding.pointSeekBar
+        val pointTV = binding.pointTV
+        pointSeekBar.progress =
+            (viewModel.gameMode.value!!.pointsToWin ?: DEFAULT_POINTS_TO_WIN).toFloat()
+        pointTV.text = pointSeekBar.progress.toInt().toString()
+        viewModel.setPointsToWin(DEFAULT_POINTS_TO_WIN)
+        pointSeekBar.setOnSeekBarChangeListener(
+            object : CircularSeekBar.OnCircularSeekBarChangeListener {
+                override fun onProgressChanged(
+                    circularSeekBar: CircularSeekBar?,
+                    progress: Float,
+                    fromUser: Boolean
+                ) {
+                    val progressToShow = progress - (progress % 5)
+                    pointSeekBar.progress = progressToShow
+                    showProgressOnTV(progressToShow.toInt())
+                }
+
+                override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: CircularSeekBar?) {
+                    if (seekBar != null) {
+                        if (seekBar.progress.toInt() < 5) {
+                            seekBar.progress = 5F
+                            showProgressOnTV(5)
+                        }
+                        viewModel.setPointsToWin(pointTV.text.toString().toInt())
+                    }
+                }
+
+                fun showProgressOnTV(progress: Int) {
+                    pointTV.text = progress.toString()
+                }
+            }
+        )
+    }
+
+    private fun initTimeSeekBar() {
+        val timeSeekBar = binding.timeSeekBar
+        val timeTV = binding.timeTV
+        timeSeekBar.progress =
+            (viewModel.gameMode.value!!.timePerRound ?: DEFAULT_TIME_PER_ROUND).toFloat()
+        timeTV.text = timeSeekBar.progress.toInt().toString()
+        viewModel.setTimePerRound(DEFAULT_TIME_PER_ROUND)
+        timeSeekBar.setOnSeekBarChangeListener(
+            object : CircularSeekBar.OnCircularSeekBarChangeListener {
+                override fun onProgressChanged(
+                    circularSeekBar: CircularSeekBar?,
+                    progress: Float,
+                    fromUser: Boolean
+                ) {
+                    val progressToShow = progress - (progress % 5)
+                    timeSeekBar.progress = progressToShow
+                    showProgressOnTV(progressToShow.toInt())
+                }
+
+                override fun onStartTrackingTouch(seekBar: CircularSeekBar?) {}
+
+                override fun onStopTrackingTouch(seekBar: CircularSeekBar?) {
+                    if (seekBar != null) {
+                        if (seekBar.progress.toInt() < 5) {
+                            seekBar.progress = 5F
+                            showProgressOnTV(5)
+                        }
+                        viewModel.setTimePerRound(timeTV.text.toString().toInt())
+                    }
+                }
+
+                fun showProgressOnTV(progress: Int) {
+                    timeTV.text = progress.toString()
+                }
+            }
+        )
     }
 
     private fun initObservers() {
@@ -42,53 +120,13 @@ class PagerTimeAndPointsFragment :
         }
     }
 
-    private fun initTextViews() = with(binding) {
-        tvTime.text = (viewModel.gameMode.value!!.timePerRound ?: DEFAULT_TIME_PER_ROUND).toString()
-        tvPoints.text = (viewModel.gameMode.value!!.pointsToWin ?: DEFAULT_POINTS_TO_WIN).toString()
-
-        viewModel.setTimePerRound(tvTime.text.toString().toInt())
-        viewModel.setPointsToWin(tvPoints.text.toString().toInt())
-    }
-
     private fun initOnClickListeners() = with(binding) {
         btnClassic.setOnClickListener { viewModel.setIsClassic(true) }
         btnArcade.setOnClickListener { viewModel.setIsClassic(false) }
-
-        btnIncreaseTime.setOnClickListener {
-            val time = tvTime.text.toString().toInt()
-            val increased = time + 5
-            tvTime.text = increased.toString()
-            viewModel.setTimePerRound(increased)
-        }
-
-        btnDecreaseTime.setOnClickListener {
-            val time = tvTime.text.toString().toInt()
-            if (time > 5) {
-                val decreased = time - 5
-                tvTime.text = decreased.toString()
-                viewModel.setTimePerRound(decreased)
-            }
-        }
-
-        btnIncreasePoints.setOnClickListener {
-            val points = tvPoints.text.toString().toInt()
-            val increased = points + 5
-            tvPoints.text = increased.toString()
-            viewModel.setPointsToWin(increased)
-        }
-
-        btnDecreasePoints.setOnClickListener {
-            val points = tvPoints.text.toString().toInt()
-            if (points > 5) {
-                val decreased = points - 5
-                tvPoints.text = decreased.toString()
-                viewModel.setPointsToWin(decreased)
-            }
-        }
     }
 
     companion object {
         private const val DEFAULT_TIME_PER_ROUND = 60
-        private const val DEFAULT_POINTS_TO_WIN = 30
+        private const val DEFAULT_POINTS_TO_WIN = 35
     }
 }
